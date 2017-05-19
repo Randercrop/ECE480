e92c793720c31c7df2eb9a1312b40c6a05e94f53
pitabread <pln123@yahoo.com>
2017-05-19 16:07:08 -0700

    Add files via upload
    lol bunny

---
+++ BunCard.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "Cards/BunCard", menuName = "BunCard")]
+public class BunCard : ColoredCard
+{
+    public override void OnPlay()
+    {
+        Debug.Log("victory");
+        GameManager.victoryUI = GameManager.createUI("player " + (GameManager.PlayerIndex + 1) + " wins", "restart");
+        GameManager.victoryUI.GetComponentInChildren<Button>().onClick.AddListener(GameManager.StartNewGame);
+
+    }
+}

5fe25ebaa6249c7c47bd42281b548423b62d1653
Andrew Nguyen <yuudo1995@hotmail.com>
2017-05-19 13:49:26 -0700

    Add files via upload

---
+++ PlusTwoCard.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "Cards/PlusThreeCard", menuName = "PlusThreeCard")]
+public class PlusTwoCard : ColoredCard {
+    public override void OnPlay() {
+
+        base.OnPlay();
+        Pile.PullCard(3);
+    }
+
+    public override bool CanBePlayed()
+    {
+        return base.CanBePlayed();
+    }
+}

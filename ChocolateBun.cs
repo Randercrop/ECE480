1eb357d987e3bdb1812efd675dc9ea7a56245695
Andrew Nguyen <yuudo1995@hotmail.com>
2017-05-19 19:06:31 -0700

    Add files via upload

---
+++ ChocolateBun.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Cards/NewBlockCard", menuName = "BlockCard")]
+public class BlockCard : ColoredCard {
+    public override void OnPlay()
+    {
+        
+        for (int i = 0; i < GameManager.NextPlayer.hand.Count; i++)
+        {
+            //CardView view = GameManager.NextPlayer.hand[i].GetComponent<CardView>();
+            //GameManager.NextPlayer.hand.Remove(view);
+            //GameManager.NextPlayer.hand[i].transform.SetParent(GameManager.NextPlayer.HandGameObject.transform);
+            Destroy(GameManager.NextPlayer.hand[i]);
+        }
+        GameManager.NextPlayer.hand.Clear();
+        //GameManager.ToggleNextPlayer(true);
+        GameManager.EndTurn();
+    }
+}

21a40b65a4641d2ee42a6ff2b9e64ca38b75beed
Andrew Nguyen <yuudo1995@hotmail.com>
2017-05-25 15:57:13 -0700

    Update ChocolateBun.cs

--- ChocolateBun.cs
+++ ChocolateBun.cs
@@ -9,13 +9,10 @@ public class ChocolateBunCard : ColoredCard {
         
         for (int i = 0; i < GameManager.NextPlayer.hand.Count; i++)
         {
-            //CardView view = GameManager.NextPlayer.hand[i].GetComponent<CardView>();
-            //GameManager.NextPlayer.hand.Remove(view);
-            //GameManager.NextPlayer.hand[i].transform.SetParent(GameManager.NextPlayer.HandGameObject.transform);
+            Destroy(GameManager.NextPlayer.hand[i].CardFace);
+            Destroy(GameManager.NextPlayer.hand[i].gameObject);
             Destroy(GameManager.NextPlayer.hand[i]);
         }
-        GameManager.NextPlayer.hand.Clear();
-        //GameManager.ToggleNextPlayer(true);
         GameManager.EndTurn();
     }
 }

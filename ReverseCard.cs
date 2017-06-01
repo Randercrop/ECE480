08e68b8cd0bdbe6905cd748a4911f28a3fdb8ecf
ericchuong <eric.chuong@yahoo.com>
2017-05-31 20:17:44 -0700

    Reverse Card class

---
+++ ReverseCard.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[CreateAssetMenu(fileName = "Cards/NewReverseCard", menuName = "ReverseCard")]
+public class ReverseCard : ColoredCard
+{
+    public override bool CanBePlayed()
+    {
+        return (base.CanBePlayed());
+    }
+    public override void OnPlay()
+    {
+        base.OnPlay();
+
+        GameManager.CurrentPlayer.HandGameObject.SetActive(false);
+        GameManager.ToggleReversePlayer();
+        GameManager.EndTurn();
+    }
+}
+
+/*
+ * PUT IN GAMEMANAGER.CS
+ 
+    public static void ToggleReversePlayer(bool nextPlayer = true)
+    {
+        if (nextPlayer)
+        {
+            PlayerIndex--;
+            PlayerIndex--;
+        }
+        else
+            playerIndex--;
+
+        BeginTurn();
+    }
+
+*/
